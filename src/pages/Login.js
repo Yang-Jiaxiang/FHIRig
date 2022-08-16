1a5e2a270951e6816059b9adf39c123eb65bdafc
Yang-Jiaxiang <yang900412@gmail.com>
2022-08-17 00:53:05 +0800

    fix: 新增簡單登入

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ const Login = () => {
     return (
         <div>
             <h1>Login</h1>
+            <button onClick={()=>window.location.href="index"}>Login</button>
         </div>
     );
 }
